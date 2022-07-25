4250c79dc9f4a025cb884a0b0185de103bf3c13b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-07-25 16:06:07 +0430

    Sound Board: create Markup, style and script and it works.

---
+++ 09_sound_board_project/public/script.js
@@ -0,0 +1,23 @@
+const sounds = ["applause", "boo", "gasp", "tada", "victory", "wrong"];
+const buttons = document.getElementById("buttons");
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.className = "btn";
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+    document.getElementById(sound).play();
+  });
+
+  buttons.appendChild(btn);
+});
+
+function stopSongs() {
+  sounds.forEach((sound) => {
+    const song = document.getElementById(sound);
+    song.pause();
+    song.currentTime = 0;
+  });
+}
