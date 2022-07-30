32ce03a62c7d9f05767adbd748685414f220060b
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-07-30 04:31:51 +0430

    Random choice picker: complete the script and fix instructor's errors.

--- 13_random_choice_picker/public/script.js
+++ 13_random_choice_picker/public/script.js
@@ -4,9 +4,12 @@ const textarea = document.getElementById("textarea");
 textarea.focus();
 
 textarea.addEventListener("keyup", (e) => {
-  // TODO: check for 'enter', if it is then choose a random tag (if there's
-  // one)
-  createTags(e.target.value);
+  if (e.key === "Enter") {
+    e.target.value = "";
+    randomSelect();
+  } else {
+    createTags(e.target.value);
+  }
 });
 
 function createTags(input) {
@@ -20,7 +23,54 @@ function createTags(input) {
   tags.forEach((tag) => {
     const tagEl = document.createElement("span");
     tagEl.classList.add("tag");
-    tagEl.innerHTML = tag;
+    tagEl.innerText = tag;
     tagsEl.appendChild(tagEl);
   });
+}
+
+function randomSelect() {
+  const tags = document.querySelectorAll(".tag");
+
+  if (!tags.length) return;
+  else if (tags.length === 1) {
+    highlightTag(tags[0]);
+    return;
+  }
+
+  const times = 30;
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag();
+
+    if (randomTag) {
+      highlightTag(randomTag);
+
+      setTimeout(() => {
+        unHighlightTag(randomTag);
+      }, 100);
+    }
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval);
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag();
+
+      highlightTag(randomTag);
+    }, 100);
+  }, times * 100);
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll(".tag");
+  return tags[Math.floor(Math.random() * tags.length)];
+}
+
+function highlightTag(tag) {
+  tag.classList.add("highlight");
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove("highlight");
 }
