1c2745dc737a6434fb0b5da26378a20078ff0c68
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-02 08:18:32 +0430

    Drink water: complete the script to update the big cup.

--- 16_drink_water/public/script.js
+++ 16_drink_water/public/script.js
@@ -1,4 +1,9 @@
 const smallCups = document.querySelectorAll(".cup-small");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+const liters = document.getElementById("liters");
+
+updateBigCup();
 
 smallCups.forEach((cup, idx) => {
   cup.addEventListener("click", () => highlightCup(idx));
@@ -19,4 +24,30 @@ function highlightCup(idx) {
       c.classList.remove("full");
     }
   });
+
+  updateBigCup();
+}
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (!fullCups) {
+    percentage.style.visibility = "hidden";
+    percentage.style.hight = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    // NOTE: 330 is the height of cup
+    percentage.style.height = `${(fullCups / totalCups) * 330}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    // NOTE: 250 for ml and 1000 for liters
+    liters.innerText = `${2 - (250 * fullCups) / 1000}L`;
+  }
 }
