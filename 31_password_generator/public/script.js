9985719b80a024dca63eec1c6d40263e44c77112
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-18 13:21:58 +0430

    Password Generator: create a function that generates a random (shuffled) string of password.

--- 31_password_generator/public/script.js
+++ 31_password_generator/public/script.js
@@ -1,3 +1,12 @@
+const resultEl = document.getElementById("result");
+const lengthEl = document.getElementById("length");
+const upperEl = document.getElementById("uppercase");
+const lowerEl = document.getElementById("lowercase");
+const numberEl = document.getElementById("numbers");
+const symbolEl = document.getElementById("symbols");
+const clipboardEl = document.getElementById("clipboard");
+const generateEl = document.getElementById("generate");
+
 const random = {
   lower() {
     return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
@@ -13,3 +22,58 @@ const random = {
     return Math.floor(Math.random() * 10);
   },
 };
+
+generateEl.addEventListener("click", () => {
+  const length = parseInt(lengthEl.value);
+  const hasUpper = upperEl.checked;
+  const hasLower = lowerEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+
+  resultEl.innerText = generateRandomPassword(
+    length,
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol
+  );
+});
+
+function generateRandomPassword(
+  length,
+  hasUpper,
+  hasLower,
+  hasNumber,
+  hasSymbol
+) {
+  if (length < 4 || (!hasUpper && !hasLower && !hasNumber && !hasSymbol)) {
+    return "";
+  }
+
+  let result = "";
+  const actions = [];
+  hasLower && actions.push(random.lower);
+  hasSymbol && actions.push(random.symbol);
+  hasUpper && actions.push(random.upper);
+  hasNumber && actions.push(random.number);
+
+  for (let i = 0; i < length - actions.length; i++) {
+    result += actions[Math.floor(Math.random() * actions.length)]();
+  }
+  // to be sure that we have what we want (i.e. numbers, symbols, ...)
+  actions.forEach((act) => (result += act()));
+
+  result = shuffle(result);
+  return result;
+}
+
+function shuffle(str) {
+  let splitted = str.split("");
+  let result = "";
+
+  while (splitted.length) {
+    result += splitted.splice(Math.random() * splitted.length, 1);
+  }
+
+  return result;
+}
