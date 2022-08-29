e8a1eebac2014a52318db8bad58260d053089532
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-29 13:03:12 +0430

    Verfiy Account UI: complete HTML, CSS and JS.

--- 40_3D_background/public/script.js
+++ 40_3D_background/public/script.js
@@ -1,12 +1,12 @@
-const boxes = document.getElementById("boxes")
-const btn = document.getElementById("btn")
+const boxes = document.getElementById("boxes");
+const btn = document.getElementById("btn");
 
 function createBoxes() {
   for (let i = 0; i < 4; i++) {
     for (let j = 0; j < 4; j++) {
       const box = document.createElement("div");
       box.classList.add("box");
-      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`;
       boxes.appendChild(box);
     }
   }
@@ -14,5 +14,5 @@ function createBoxes() {
 
 createBoxes();
 
-btn.addEventListener("click", () => boxes.classList.toggle("big"))
+btn.addEventListener("click", () => boxes.classList.toggle("big"));
 // TODO: (Mustafa) run prettier before final commit
