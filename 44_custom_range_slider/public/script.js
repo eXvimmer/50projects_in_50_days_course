9effd86d6385f34f0ff57c9e8cdec5a0741e2038
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-01 04:07:47 +0430

    Custom range slider: add script to handle the slider and the label.

--- 44_custom_range_slider/public/script.js
+++ 44_custom_range_slider/public/script.js
@@ -1,1 +1,27 @@
-// TODO: run prettier before commits or make it automated
+const range = document.getElementById("range");
+
+range.addEventListener("input", e => {
+  const value = +e.target.value;
+  const label = e.target.nextElementSibling;
+
+  const rangeWidth = getComputedStyle(e.target).getPropertyValue("width");
+  const labelWidth = getComputedStyle(label).getPropertyValue("width");
+
+  const numRangeWidth = +rangeWidth.substring(0, rangeWidth.length - 2);
+  const numLabelWidth = +labelWidth.substring(0, labelWidth.length - 2);
+
+  const min = +e.target.min;
+  const max = +e.target.max;
+
+  const left =
+    value * (numRangeWidth / max) -
+    numLabelWidth / 2 +
+    scale(value, min, max, 10, -10);
+  label.style.left = `${left}px`;
+
+  label.innerText = value;
+});
+
+function scale(num, inMin, inMax, outMin, outMax) {
+  return ((num - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+}
