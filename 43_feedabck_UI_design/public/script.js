e4e2bdb89b00a7adb71f48270e09f7ddf16069db
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-31 08:18:57 +0430

    Feedback UI design: add script to handle events.

--- 43_feedabck_UI_design/public/script.js
+++ 43_feedabck_UI_design/public/script.js
@@ -1,1 +1,30 @@
-// TODO: run prettier before commits or make it automated
+const ratings = document.querySelectorAll(".rating");
+const ratingsContainer = document.querySelector(".ratings-container");
+const sendBtn = document.getElementById("send");
+const panel = document.getElementById("panel");
+let selectedRating = "Satisfied";
+
+ratingsContainer.addEventListener("click", e => {
+  if (e.target.parentNode.classList.contains("rating")) {
+    removeActive();
+    e.target.parentNode.classList.add("active");
+    selectedRating = e.target.nextElementSibling.innerHTML;
+  }
+});
+
+sendBtn.addEventListener("click", () => {
+  panel.innerHTML = `
+    <i class="fas fa-heart"></i>
+    <strong>Thank You!</strong>
+    <br>
+    <strong>Feedback ${selectedRating}</strong>
+    <p>We'll use your feedback to improve our customer support.</p>
+  `;
+});
+
+function removeActive() {
+  // ratings.forEach(rating => rating.classList.remove("active"))
+  for (let i = 0; i < ratings.length; i++) {
+    ratings[i].classList.remove("active");
+  }
+}
