b71b88da82a67da91d6e0ce41d33496d555dd5e4
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-02 15:06:40 +0430

    Netflix Mobile Navigation: add script and css delays to make navigation section look better.

--- 45_netflix_navigation/public/script.js
+++ 45_netflix_navigation/public/script.js
@@ -1,1 +1,11 @@
-// TODO: run prettier before commits or make it automated
+const openBtn = document.querySelector(".open-btn");
+const closeBtn = document.querySelector(".close-btn");
+const nav = document.querySelectorAll(".nav");
+
+openBtn.addEventListener("click", () => {
+  nav.forEach(el => el.classList.add("visible"));
+});
+
+closeBtn.addEventListener("click", () => {
+  nav.forEach(el => el.classList.remove("visible"));
+});
