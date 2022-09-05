e01a3147559bcce65a6865f1bdeba6fd1743e5fb
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-09-05 20:35:42 +0430

    Todo List: Add, remove and mark a todo.

--- 49_todo_list/public/script.js
+++ 49_todo_list/public/script.js
@@ -1,1 +1,42 @@
-// TODO: run prettier before commits or make it automated
+const form = document.getElementById("form");
+const input = document.getElementById("input");
+const todosList = document.getElementById("todos");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  addTodo();
+});
+
+function addTodo(todo) {
+  let todoText = input.value;
+
+  if (todo) {
+    todoText = todo.text;
+  }
+
+  if (todoText) {
+    const todoEl = document.createElement("li");
+    if (todo && todo.completed) {
+      todoEl.classList.add("completed");
+    }
+
+    todoEl.innerText = todoText;
+
+    todoEl.addEventListener("click", () => {
+      todoEl.classList.toggle("completed");
+      input.focus();
+    });
+
+    todoEl.addEventListener("contextmenu", e => {
+      e.preventDefault();
+      todoEl.remove();
+      input.focus();
+    });
+
+    todosList.appendChild(todoEl);
+    input.value = "";
+
+    // TODO: Add todos to localStorage
+    input.focus();
+  }
+}
